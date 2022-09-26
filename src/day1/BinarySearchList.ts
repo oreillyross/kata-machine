1472bbdce42c70e365302493950597213945865e
Ross O'Reilly <oreillyross@gmail.com>
2022-09-26 05:07:35 +0200

    Bubble sort

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,23 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+        let low = 0 
+	let high = haystack.length  
+
+	while (low < high)  {
+		const mid = Math.floor(low + (high - low) / 2)
+		const val = haystack[mid]
+		
+		if (val === needle) {
+			return true
+		} else if (needle > val) {
+			low = mid + 1
+		} else {
+			high = mid 
+		}
+
+	} 
+
+
+	return false
+
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+	const jmp = Math.floor(Math.sqrt(breaks.length))
+	for (let i = 0; i < breaks.length; i += jmp) {
+		if (breaks[i] === true) {
+			i -= jmp
+			for (let j = i; j < breaks.length; j++) {
+				if (breaks[j] === true) {
+					return j
+				}
+			}
+		}
+	}
+	return -1
+
+
 }
